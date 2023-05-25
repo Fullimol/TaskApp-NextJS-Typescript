17e396e63fe85aa2820c8ed174107ba2681b78b1
Bruno Freijomil <bruno_freijomil@hotmail.com>
2023-05-25 16:13:22 -0300

    ADD footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,37 +2,50 @@ import { useContext } from 'react'
 import { Context } from '../context/ContextProvider'
 import { BsFillTrash3Fill } from 'react-icons/bs'
 import { useRouter } from 'next/router'
+import { AiOutlinePlus } from 'react-icons/ai'
 
 const Home = () => {
   const { tasks, deleteTask } = useContext(Context)
-  const router = useRouter()
+  const router = useRouter() //agrego esto para darla función al boton y que me redirija.
 
   return (
-    <div className='flex justify-center align-center'>
-      {tasks.length === 0 ? (<h1>No tasks</h1>) : (
-        <div className='w-7/12'  >
-          {tasks.map((task, index) => (
-            
-            <div key={task.id} className={`bg-gray-700 hover:bg-gray-600 cursor-pointer px-20 py-5 m-2 flex justify-start items-center`} onClick={() => router.push(`edit/${task.id}`)}>
-              <span className='text-5xl mr-5'>{index + 1}</span>
-              <div className='w-full'>
-                <div className='flex justify-between'>
-                  <h1 className='font-bold'>{task.title}</h1>
+    <div>
 
-                  <button className='rounded-xl bg-red-700 hover:bg-red-600 px-3 py-1 flex items-center' onClick={(e) => {
-                    e.stopPropagation();
-                    deleteTask(task.id)
-                  }}>
-                    <BsFillTrash3Fill className='mr-2' /> Delete </button>
-                </div>
-                <p className='text-gray-300'>{task.description}</p>
-                <span className='text-gray-500 text-sm'>id: {task.id}</span>
-              </div>
-            </div>
-          ))
-          }
+      <div>
+        <div className='flex justify-center align-center mb-5'>
+          <button className='transition hover:scale-110 hover:bg-green-500 bg-green-700 px-3 py-2 rounded-xl inline-flex items-center' onClick={() => router.push("/new")}>
+          <AiOutlinePlus className='mr-2' />
+            {"New Task"}
+          </button>
         </div>
-      )}
+      </div>
+
+      <div className='flex justify-center align-center'>
+        {tasks.length === 0 ? (<h1 style={{marginTop:"200px"}}>(No tasks)</h1>) : (
+          <div className='w-7/12'  >
+            {tasks.map((task, index) => (
+
+              <div key={task.id} className={`bg-gray-700 hover:bg-gray-600 cursor-pointer px-20 py-5 m-2 flex justify-start items-center`} onClick={() => router.push(`edit/${task.id}`)}>
+                <span className='text-5xl mr-5'>{index + 1}</span>
+                <div className='w-full'>
+                  <div className='flex justify-between'>
+                    <h1 className='font-bold'>{task.title}</h1>
+
+                    <button className='rounded-xl bg-red-700 hover:bg-red-600 px-3 py-1 flex items-center' onClick={(e) => {
+                      e.stopPropagation();
+                      deleteTask(task.id)
+                    }}>
+                      <BsFillTrash3Fill className='mr-2' /> Delete </button>
+                  </div>
+                  <p className='text-gray-300'>{task.description}</p>
+                  <span className='text-gray-500 text-sm'>id: {task.id}</span>
+                </div>
+              </div>
+            ))
+            }
+          </div>
+        )}
+      </div>
     </div>
   )
 }
